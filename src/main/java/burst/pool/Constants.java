334194a0eacf3c82f69e8961c300f4b408928d95
jjos2372 <jjos2372@gmail.com>
2020-07-09 16:38:00 -0300

    New version and project name.

--- src/main/java/burst/pool/Constants.java
+++ src/main/java/burst/pool/Constants.java
@@ -1,6 +1,6 @@
 package burst.pool;
 
 public class Constants {
-    public static final String VERSION = "v1.3.0";
-    public static final String USER_AGENT = "bat-burstpool/" + VERSION;
+    public static final String VERSION = "v1.3.1";
+    public static final String USER_AGENT = "babel-pool/" + VERSION;
 }
