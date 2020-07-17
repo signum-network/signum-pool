7fafbd63cb796c635da4078b936620f24845ea26
jjos2372 <jjos2372@gmail.com>
2020-07-17 09:35:17 -0300

    New version.

--- src/main/java/burst/pool/Constants.java
+++ src/main/java/burst/pool/Constants.java
@@ -1,6 +1,6 @@
 package burst.pool;
 
 public class Constants {
-    public static final String VERSION = "v1.4.0";
+    public static final String VERSION = "v1.4.1";
     public static final String USER_AGENT = "babel-pool/" + VERSION;
 }
