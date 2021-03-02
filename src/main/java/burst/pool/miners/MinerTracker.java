3d18a05b7831f4f52f682381a8338fa7130f4291
jjos2372 <jjos2372@gmail.com>
2021-03-02 12:08:18 -0300

    Another poc+ fix

--- src/main/java/burst/pool/miners/MinerTracker.java
+++ src/main/java/burst/pool/miners/MinerTracker.java
@@ -62,7 +62,7 @@ public class MinerTracker {
             // PoC+ logic
             BurstValue commitment = miner.getCommitment();
             
-            double commitmentFactor = commitment.doubleValue()/miningInfo.getAverageCommitment();
+            double commitmentFactor = ((double)commitment.longValue())/miningInfo.getAverageCommitment();
             commitmentFactor = Math.pow(commitmentFactor, 0.4515449935);
             commitmentFactor = Math.min(8.0, commitmentFactor);
             commitmentFactor = Math.max(0.125, commitmentFactor);
