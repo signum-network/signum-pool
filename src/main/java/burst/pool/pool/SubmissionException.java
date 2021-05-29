b89545d66e9e43ee5c53f758342d83c87843b98f
jjos2372 <jjos2372@gmail.com>
2021-05-29 19:44:58 -0300

    Remove warning

--- src/main/java/burst/pool/pool/SubmissionException.java
+++ src/main/java/burst/pool/pool/SubmissionException.java
@@ -1,6 +1,8 @@
 package burst.pool.pool;
 
 public class SubmissionException extends Exception {
+    private static final long serialVersionUID = 2974403357208404084L;
+
     public SubmissionException() {
     }
 
