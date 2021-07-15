9e01cf9486da74d37e025e3acd21c7e917474f48
jjos2372 <jjos2372@gmail.com>
2021-07-15 10:12:32 -0300

    Version.

--- src/main/java/burst/pool/Constants.java
+++ src/main/java/burst/pool/Constants.java
@@ -1,6 +1,6 @@
 package burst.pool;
 
 public class Constants {
-    public static final String VERSION = "v2.1.2";
+    public static final String VERSION = "v2.1.3";
     public static final String USER_AGENT = "signum-pool/" + VERSION;
 }
