159a899b39a7b108df3c4101e3d9a012685f4013
ipr0310 <35440957+ipr0310@users.noreply.github.com>
2024-06-22 00:40:54 -0400

    Self Host Fonts for privacy reasons

--- react-pool/src/main.tsx
+++ react-pool/src/main.tsx
@@ -1,3 +1,10 @@
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource-variable/montserrat";
+
 import { Provider as ReduxProvider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./states/store";
@@ -5,7 +12,6 @@ import { store } from "./states/store";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
